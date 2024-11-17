1b796d7b6f74bc7119cabdf1ab819dfbc523a184
Marcos Diaz <Diazelhalabimarcosa@gmail.com>
2024-11-17 00:13:53 -0400

    Cargado el dataframe

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pandas as pd
 
 '''Ruta de el data set'''
 file_data_path = './data/mental_health_dataset.csv'
+
+'''Carga de datos del archivo cvs a python con pandas'''
 data_health = pd.read_csv(file_data_path,sep=',')
 
-data_health.head()
+
+print(data_health.head(10))
