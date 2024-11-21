381968f1151042b49d1d3c2299934c4c2ca953d1
Marcos Diaz <Diazelhalabimarcosa@gmail.com>
2024-11-21 10:18:40 -0400

    Analisis listo falta pasar a funciones y hacer la interfaz

--- functions/severidad_de_problemas_mentales.py
+++ functions/severidad_de_problemas_mentales.py
@@ -2,16 +2,15 @@ import pandas as pd
 import plotly.express as px
 
 ''' Definicion de la funcion que muestra la grafica de la severidad de los problemas mentales de las personas'''
-def severidad_de_problemas_mentales( data_health: pd.DataFrame, query_reporte_problemas_mentales:str ) -> None:
+def severidad_de_problemas_mentales( data_health: pd.DataFrame, query_reporte_problemas_mentales:str ) -> tuple:
     
-
-
     '''Se agrupan los datos por la persona reporto problemas mentales y por la severidad'''
     severidad_de_problemas_mentales = data_health.groupby(['Mental_Health_Condition','Severity'],dropna=False).size().reset_index().rename(columns={0: "Count_Severity"})
     
     '''Grafica de la severdad de los problemas mentales de las personas, con un query de si la persona reporto problemas mentales o no'''
     grafica_severidad_de_problemas_mentales = px.pie(severidad_de_problemas_mentales.query(f"Mental_Health_Condition == '{query_reporte_problemas_mentales}'"),values="Count_Severity", names="Severity", hole=.2, title="Severidad de los problemas mentales de las personas")
     texto_analisis = ""
+    return (grafica_severidad_de_problemas_mentales, texto_analisis)
    
 
    
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import dash_bootstrap_components as dbc
+from dash import Dash, Input, Output, dcc, html
 from functions import severidad_de_problemas_mentales
+
 
 '''Ruta de el data set'''
 file_data_path = './data/mental_health_dataset.csv'
@@ -8,4 +11,44 @@ file_data_path = './data/mental_health_dataset.csv'
 data_health = pd.read_csv(file_data_path,sep=',')
 
 
+severidad_de_problemas_mentales(data_health,"Yes")
 
+
+''' Parte de la interfaz grafica'''
+# external_stylesheets = [dbc.themes.BOOTSTRAP]
+# app = Dash(__name__,external_stylesheets=external_stylesheets)
+
+# select = dbc.Select(
+#     id="select",
+#     options=[
+#         {"label": "Yes", "value": "Yes"},
+#         {"label": "No", "value": "No"},
+        
+#     ],
+# )
+
+# app.layout = dbc.Container(
+#     [
+#         html.H1("Graficas"),
+#         html.Hr(),
+#         dbc.Row(
+#             [
+#                 dbc.Col([dcc.Graph(id="Grafico_Prueba"),select]),
+#                 dbc.Col([html.H2("hola")])
+               
+#             ]
+#         ),
+       
+#     ]
+# )
+
+# @app.callback(
+#         Output(component_id="Grafico_Prueba",component_property="figure"),
+#         Input(component_id="select",component_property="value")
+# )
+# def update_graph(values):
+#     return sev.severidad_de_problemas_mentales(data_health,query_reporte_problemas_mentales=values)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+    
